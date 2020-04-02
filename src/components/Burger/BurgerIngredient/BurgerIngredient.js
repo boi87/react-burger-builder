2495b335d1a973c6ee3f77e4eacef1e2d034f195
mike <boimik87@gmail.com>
2020-04-02 12:39:03 +0100

    burger components replaced - burger showing in UI

---
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import css from './BurgerIngredient.module.css'
+
+const burgerIngredient = props => {
+    let ingredient = null;
+
+    switch (props.type) {
+        case ('bread-bottom'):
+            ingredient = <div className={css['bread-bottom']}></div>
+            break;
+        case ('bread-top'):
+            ingredient =
+                <div className={css['bread-top']}>
+                    <div className={css.seeds1}></div>
+                    <div className={css.seeds2}></div>
+                </div>
+
+            break;
+        case ('meat'):
+            ingredient = <div className={css.meat}></div>
+            break;
+        case ('cheese'):
+            ingredient = <div className={css.cheese}></div>
+            break;
+        case ('salad'):
+            ingredient = <div className={css.salad}></div>
+            break;
+        case ('bacon'):
+            ingredient = <div className={css.bacon}></div>
+            break;
+        default: ingredient = null;
+    }
+
+    return ingredient;
+
+};
+
+export default burgerIngredient;
