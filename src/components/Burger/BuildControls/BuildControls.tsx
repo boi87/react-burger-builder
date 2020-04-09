9bb001847f937691d43591c9c15f6cf7d24ae284
mike <boimik87@gmail.com>
2020-04-09 16:22:04 +0100

    inline bold price

--- src/components/Burger/BuildControls/BuildControls.tsx
+++ src/components/Burger/BuildControls/BuildControls.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import css from './BuildControls.module.css'
 import BuildControl from "./BuildControl/BuildControl";
+import {Button} from '@material-ui/core';
 
 const controls = [
     {label: 'Salad', type: 'salad'},
@@ -11,7 +12,7 @@ const controls = [
 
 const BuildControls = (props: any) => (
     <div className={css.buildControls}>
-        <p>Price: £ {(props.totalPrice / 100).toFixed(2)}</p>
+        <p>Price: <span style={{fontWeight: 'bold'}}>£ {(props.totalPrice / 100).toFixed(2)}</span></p>
         {controls.map(ctrl => (
             <BuildControl
                 key={ctrl.label}
@@ -21,7 +22,7 @@ const BuildControls = (props: any) => (
                 disabled={props.disabled[ctrl.type]}
             />
         ))}
-
+        <Button disabled={props.totalPrice === 0} variant="contained">CHECK OUT</Button>
     </div>
 );
 
