43675e542201c51e2b44d96918347f83a1fd61dd
mike <boimik87@gmail.com>
2020-04-19 22:06:50 +0100

    changed order container styling

--- src/components/Burger/OrderedSummary/OrderedSummary.tsx
+++ src/components/Burger/OrderedSummary/OrderedSummary.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
 import Auxiliary from "../../../hoc/Auxiliary";
-import {IModalProps, IOrderSummaryProps} from "../../../models/burger.models";
+import {IOrderSummaryProps} from "../../../models/burger.models";
 
 import Button from "@material-ui/core/Button";
+
+import css from './OrderSummary.module.css'
 
 class OrderedSummary extends React.Component<IOrderSummaryProps, any> {
 
@@ -18,21 +20,23 @@ class OrderedSummary extends React.Component<IOrderSummaryProps, any> {
     render() {
         return (
             <Auxiliary>
-                <h3>Your Order</h3>
-                <p>Your delicious burger contains:</p>
-                <ul>
-                    {this.ingredientsSummary}
-                </ul>
-                <p>Total price: <span style={{fontWeight: 'bold'}}>
+                <div className={css.orderContainer}>
+                    <h3>Your Order</h3>
+                    <p>Your delicious burger contains:</p>
+                    <ul>
+                        {this.ingredientsSummary}
+                    </ul>
+                    <p>Total price: <span style={{fontWeight: 'bold'}}>
                     £ {(this.props.totalPrice / 100).toFixed(2)}
                 </span>
-                </p>
-                <Button
-                    disabled={this.props.totalPrice === 0}
-                    color={"primary"}
-                    // onClick={props.continuedToPayment}
-                >Continue to payment
-                </Button>
+                    </p>
+                    <Button
+                        disabled={this.props.totalPrice === 0}
+                        style={{color: 'green'}}
+                        // onClick={props.continuedToPayment}
+                    >Continue
+                    </Button>
+                </div>
             </Auxiliary>
         )
     }
