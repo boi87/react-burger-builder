8316bd81ae24519477ba6bc0e798b337333ebdc3
mike <boimik87@gmail.com>
2020-04-24 21:49:19 +0100

    added form

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Switch, Route} from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
 import CheckOut from "./containers/CheckOut/CheckOut";
+import ContactData from "./containers/CheckOut/ContactData/ContactData";
 
 function App() {
     return (
@@ -11,7 +12,8 @@ function App() {
             <Layout>
                 <Switch>
                     <Route exact path={'/'} component={BurgerBuilder}/>
-                    <Route path={'/checkout'} component={CheckOut}/>
+                    <Route exact path={'/checkout'} component={CheckOut}/>
+                    <Route exact path={'/checkout/contact-data'} component={ContactData}/>
                 </Switch>
             </Layout>
         </div>
---
+++ src/containers/CheckOut/ContactData/ContactData.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import {Button, TextField} from "@material-ui/core";
+
+import css from './ContactData.module.css'
+
+class ContactData extends React.Component<any, any> {
+    state = {
+        name: '',
+        email: '',
+        address: {
+            street: '',
+            postCode: ''
+        }
+    };
+
+    handleChange() {
+        
+    }
+
+    render() {
+        return (
+            <div>
+                <h4>Enter your contact data</h4>
+                <form className={css.formContainer}>
+                    <TextField id="standard-basic" label="Your Name"/>
+                    <TextField id="standard-basic" label="Your Email"/>
+                    <TextField id="standard-basic" label="Street"/>
+                    <TextField id="standard-basic" label="Post code"/>
+                    <Button style={{color: 'green'}}>ORDER</Button>
+                </form>
+            </div>
+        );
+    }
+};
+
+export default ContactData;
